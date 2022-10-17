e5a516310d47110ccda507df65c269ca8fafdbac
Hassan Masood <iamhassanmasood@gmail.com>
2022-10-17 21:25:01 +0500

    form:animated login and signup created

---
+++ src/Forms.js
@@ -0,0 +1,61 @@
+import React, { useRef } from "react";
+import "./Forms.css";
+
+const Forms = () => {
+  const container = useRef();
+
+  const onSignUp = () => {
+    container.current.classList.add("right-panel-active");
+  };
+
+  const onSignIn = () => {
+    container.current.classList.remove("right-panel-active");
+  };
+
+  return (
+    <div className="container" id="container" ref={container}>
+      <div className="form-container sign-up-container">
+        <form action="#">
+          <h1 className="title-class">Create Account</h1>
+
+          <input type="text" placeholder="Name" />
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <button onClick={onSignUp}>Sign Up</button>
+        </form>
+      </div>
+      <div className="form-container sign-in-container">
+        <form action="#">
+          <h1 className="title-class">Sign in</h1>
+
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <a href="#">Forgot your password?</a>
+          <button onClick={onSignIn}>Sign In</button>
+        </form>
+      </div>
+      <div className="overlay-container">
+        <div className="overlay">
+          <div className="overlay-panel overlay-left">
+            <h1>Welcome Back!</h1>
+            <p>
+              To keep connected with us please login with your personal info
+            </p>
+            <button className="ghost" id="signIn" onClick={onSignIn}>
+              Sign In
+            </button>
+          </div>
+          <div className="overlay-panel overlay-right">
+            <h1>Hello, Friend!</h1>
+            <p>Enter your personal details and start journey with us</p>
+            <button className="ghost" id="signUp" onClick={onSignUp}>
+              Sign Up
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Forms;
